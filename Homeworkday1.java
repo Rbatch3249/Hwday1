07751a0305cce7f8b529f4d63eaf4158bd58f8d0
Ryan <rbatchelor3249@gmail.com>
2020-09-01 17:32:18 -0400

    This is homework day1

---
+++ Homeworkday1.java
@@ -0,0 +1,48 @@
+public class Homeworkday1 {
+
+    public static void main(String[] args) {
+        float onlyFans = 10.99F;
+        float tax = 2.34F;
+        System.out.println(onlyFans+ tax);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
